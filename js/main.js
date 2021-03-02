a7a8fca1383c526335f595b59949e1e08876e467
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-03-02 13:49:44 +0100

    added pictures & text

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 const image = document.getElementById("slider_image");
-let imgArray = ["img/small1.jpg", "img/small2.jpg", "img/small3.jpg"];
+let imgArray = [
+    "img/vintønden.1.jpg",
+    "img/vintønden.3.jpg",
+    "img/vintønden.2.jpg",
+    "img/vintønden.4.jpg",
+];
+let imgDuration = 4000;
 let curIndex = 0;
-let imgDuration = 2000;
 
 function slideShow() {
     image.src = imgArray[curIndex];
     curIndex++;
+
     if (curIndex == imgArray.length) {
         curIndex = 0;
     }
