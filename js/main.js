0763d7bacad33aecbdedd16283852346412cfb7a
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-03-17 14:58:16 +0100

    SOME Page work

--- js/main.js
+++ js/main.js
@@ -1,37 +1,14 @@
-// Image slider section
-
-// Variabel til img objekt
-const image = document.getElementById("slider_image");
-
-// Array med src atributter til slider, string format
-let imgArray = [
-    "img/vintønden.1.webp",
-    "img/vintønden.3.webp",
-    "img/vintønden.2.webp",
-    "img/vintønden.4.webp",
-];
-
-// Variabel for startindex i slider
-let curIndex = 0;
-
-slideShow = () => {
-    // Img objektet starter i array position 0
-    image.src = imgArray[curIndex];
-    // Tæller 1 op, maks er 3
-    curIndex++;
-    // Hvis tælleren rammer arraylængden (4), sættes den tilbage til 0, starter forfra
-    if (curIndex == imgArray.length) {
-        curIndex = 0;
-    }
-    // Funktion kaldes hvert 4. sekund
-    setTimeout("slideShow()", 4000);
+// this function will check whether a tag is visible
+isElemVisible = (elem) => {
+    return (
+        elem.getBoundingClientRect().top >= 0 &&
+        elem.getBoundingClientRect().bottom < window.innerHeight
+    );
 };
-slideShow();
-
-/*--------------------------------------------------*/
 
 // Video section
 // Variabel til video objekt
+
 const promo = document.querySelector(".promo_video");
 /* 
 Funktion tjekker når der scrolles på siden,
@@ -46,14 +23,9 @@ irriterende.
 */
 if (screen.width > 1024) {
     window.onscroll = () => {
-        /*
-        promo.getBoundingClientRect().bottom < 250 ||
-        promo.getBoundingClientRect().top > 250
-            ? promo.pause()
-            : promo.play();
-    };
-    */
-        if (isElemVisible(promo)) {
+        if (promo == null) {
+            return;
+        } else if (isElemVisible(promo)) {
             promo.play();
         } else {
             promo.pause();
@@ -72,10 +44,35 @@ window.addEventListener("scroll", () => {
     });
 });
 
-// this function will check whether a tag is visible
-isElemVisible = (elem) => {
-    return (
-        elem.getBoundingClientRect().top >= 0 &&
-        elem.getBoundingClientRect().bottom < window.innerHeight
-    );
+// Image slider section
+
+// Variabel til img objekt
+const image = document.getElementById("slider_image");
+
+// Array med src atributter til slider, string format
+let imgArray = [
+    "img/vintønden.1.webp",
+    "img/vintønden.3.webp",
+    "img/vintønden.2.webp",
+    "img/vintønden.4.webp",
+];
+
+// Variabel for startindex i slider
+let curIndex = 0;
+
+slideShow = () => {
+    // Img objektet starter i array position 0
+    if (image == null) {
+        return;
+    }
+    image.src = imgArray[curIndex];
+    // Tæller 1 op, maks er 3
+    curIndex++;
+    // Hvis tælleren rammer arraylængden (4), sættes den tilbage til 0, starter forfra
+    if (curIndex == imgArray.length) {
+        curIndex = 0;
+    }
+    // Funktion kaldes hvert 4. sekund
+    setTimeout("slideShow()", 4000);
 };
+slideShow();
